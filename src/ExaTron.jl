35c6f4cf9e8c45bd20df04e877b3b9a5e31b6427
Michel Schanen <mschanen@anl.gov>
2022-03-07 11:05:05 -0600

    Initial commit

---
+++ src/ExaTron.jl
@@ -0,0 +1,60 @@
+module ExaTron
+
+using Libdl
+using LinearAlgebra
+using CUDA
+using CUDA.CUBLAS
+
+using MPI
+
+using Printf
+using DelimitedFiles
+
+export dtron, solveProblem, createProblem, setOption, getOption, ExaTronProblem
+
+const BLAS_LIBRARY = :Tron
+const EXATRON_LIBRARY = "libtron"
+
+has_c_library() = !isnothing(Libdl.dlopen(EXATRON_LIBRARY; throw_error=false))
+tron_zeros(S, n) = fill!(S(undef, Int64(n)), zero(eltype(S)))
+tron_zeros(S, dims::Tuple) = fill!(S(undef, Int64(dims[1]), Int64(dims[1])), zero(eltype(S)))
+
+include("daxpy.jl")
+include("dcopy.jl")
+include("ddot.jl")
+include("dmid.jl")
+include("dnrm2.jl")
+include("dgpnorm.jl")
+include("dscal.jl")
+include("dsel2.jl")
+include("dssyax.jl")
+include("ihsort.jl")
+include("insort.jl")
+include("dnsol.jl")
+include("dtsol.jl")
+include("dtrqsol.jl")
+include("dbreakpt.jl")
+include("dgpstep.jl")
+include("dicf.jl")
+include("dicfs.jl")
+include("dprsrch.jl")
+include("dcauchy.jl")
+include("dtrpcg.jl")
+include("dspcg.jl")
+include("dtron.jl")
+include("driver.jl")
+
+include("admm/opfdata.jl")
+include("admm/environment.jl")
+include("admm/proxal_wrapper.jl")
+include("admm/bus_kernel.jl")
+include("admm/generator_kernel.jl")
+include("admm/generator_kernel_proxal.jl")
+include("admm/polar_kernel.jl")
+include("admm/tron_kernel.jl")
+include("admm/eval_kernel.jl")
+include("admm/auglag_kernel.jl")
+include("admm/acopf_admm_gpu.jl")
+include("admm/acopf_admm_gpu_two_level.jl")
+
+end # module
---
+++ src/driver.jl
@@ -0,0 +1,298 @@
+mutable struct ExaTronProblem{VI, VD, TM <: AbstractTronMatrix}
+    n::Cint                 # number of variables
+    nnz::Integer            # number of Hessian entries
+    nnz_a::Integer          # number of Hessian entries in the strict lower
+    A::TM
+    B::TM
+    L::TM
+    indfree::VI   # a working array of dimension n
+    iwa::VI       # a working array of dimension 3*n
+    g::VD         # gradient
+    xc::VD        # a working array of dimension n
+    s::VD         # a working array of dimension n
+    wa::VD        # a working array of dimension 7*n
+
+    x::VD
+    x_l::VD
+    x_u::VD
+    rows::VI
+    cols::VI
+    values::VD
+
+    eval_f::Function
+    eval_grad_f::Function
+    eval_h::Function
+
+    options::Dict{String, Any}
+
+    # Statistics
+    delta::Cdouble
+    f::Cdouble
+    gnorm_two::Float64
+    gnorm_inf::Float64
+    nfev::Integer
+    ngev::Integer
+    nhev::Integer
+    minor_iter::Integer
+    status::Symbol
+
+    ExaTronProblem{VI, VD, TM}() where {VI, VD, TM} = new{VI, VD, TM}()
+end
+
+function gpnorm(n, x, x_l, x_u, g)
+    two_norm = 0.0
+    inf_norm = 0.0
+
+    for i=1:n
+        if x_l[i] != x_u[i]
+            if x[i] == x_l[i]
+                val = (min(g[i], 0.0))^2
+            elseif x[i] == x_u[i]
+                val = (max(g[i], 0.0))^2
+            else
+                val = g[i]^2
+            end
+
+            two_norm += val
+            val = sqrt(val)
+            if inf_norm < val
+                inf_norm = val
+            end
+        end
+    end
+
+    two_norm = sqrt(two_norm)
+
+    return two_norm, inf_norm
+end
+
+function set_default_options!(prob::ExaTronProblem)
+    prob.options = Dict{String,Any}()
+    set_default_options!(prob.options)
+    return
+end
+
+function set_default_options!(options::Dict{String, Any})
+    options["max_feval"] = 500
+    options["max_minor"] = 200
+    options["p"] = 5
+    options["verbose"] = 0
+    options["tol"] = 1e-6
+    options["fatol"] = 0.0
+    options["frtol"] = 1e-12
+    options["fmin"] = -1e32
+    options["cgtol"] = 0.1
+    options["tron_code"] = :Julia
+    options["matrix_type"] = :Sparse
+
+    return
+end
+
+function getOption(prob::ExaTronProblem, keyword::String)
+    if !haskey(prob.options, keyword)
+        error("ExaTron does not have option with name $(keyword).")
+    end
+    return prob.options[keyword]
+end
+
+function setOption(prob::ExaTronProblem, keyword::String, value::Symbol)
+    if !haskey(prob.options, keyword)
+        error("ExaTron does not have option with name $(keyword).")
+    end
+    prob.options[keyword] = value
+end
+
+function setOption(prob::ExaTronProblem, keyword::String, value::Integer)
+    if !haskey(prob.options, keyword)
+        error("ExaTron does not have option with name $(keyword).")
+    end
+    prob.options[keyword] = value
+    return
+end
+
+function setOption(prob::ExaTronProblem, keyword::String, value::Float64)
+    if !haskey(prob.options, keyword)
+        error("ExaTron does not have option with name $(keyword).")
+    end
+    prob.options[keyword] = value
+    return
+end
+
+function instantiate_memory!(tron::ExaTronProblem{VI,VD}, n, nele_hess) where {VI,VD}
+    tron.n = convert(Cint, n)
+    tron.nnz = nele_hess
+    tron.indfree = VI(undef, n)
+    tron.iwa = VI(undef, 3*n)
+
+    tron.g = tron_zeros(VD, n)
+    tron.xc = tron_zeros(VD, n)
+    tron.s = tron_zeros(VD, n)
+    tron.wa = tron_zeros(VD, 7*n)
+
+    tron.x = tron_zeros(VD, n)
+    tron.x_l = tron_zeros(VD, n)
+    tron.x_u = tron_zeros(VD, n)
+    tron.rows = VI(undef, nele_hess)
+    tron.cols = VI(undef, nele_hess)
+    tron.values = tron_zeros(VD, nele_hess)
+end
+
+function createProblem(n::Integer, x_l::AbstractVector{Float64}, x_u::AbstractVector{Float64},
+                       nele_hess::Integer, eval_f_cb, eval_grad_f_cb, eval_h_cb; options...)
+    @assert n == length(x_l) == length(x_u)
+    @assert typeof(x_l) == typeof(x_u)
+
+    # Load options first, as we need to know :matrix_type for type inference
+    options_dict = Dict{String, Any}()
+    set_default_options!(options_dict)
+    for (name, value) in options
+        options_dict[string(name)] = value
+    end
+
+    VI = Vector{Cint}
+    VD = typeof(x_l)
+
+    if options_dict["matrix_type"] == :Sparse
+        TM = TronSparseMatrixCSC{VI, VD}
+    else
+        AT = isa(x_l, Array) ? Array{Float64, 2} : CuArray{Float64, 2}
+        TM = TronDenseMatrix{AT}
+    end
+
+    tron = ExaTronProblem{VI, VD, TM}()
+    tron.options = options_dict
+    instantiate_memory!(tron, n, Int64(nele_hess))
+    copyto!(tron.x_l, 1, x_l, 1, n)
+    copyto!(tron.x_u, 1, x_u, 1, n)
+
+    tron.eval_f = eval_f_cb
+    tron.eval_grad_f = eval_grad_f_cb
+    tron.eval_h = eval_h_cb
+
+    tron.eval_h(tron.x, :Structure, tron.rows, tron.cols, 1.0, Float64[], Float64[])
+    # Instantiate sparse matrix
+    p = tron.options["p"]
+
+    if tron.options["matrix_type"] == :Sparse
+        tron.A = TronSparseMatrixCSC(tron.rows, tron.cols, tron.values, n)
+        tron.B = TronSparseMatrixCSC{VI, VD}(n, Int64(nele_hess))
+        tron.L = TronSparseMatrixCSC{VI, VD}(n, Int64(nele_hess + n*p))
+        tron.nnz_a = tron.A.nnz
+    else
+        tron.A = TronDenseMatrix(tron.rows, tron.cols, tron.values, n)
+        if isa(x_l, Array)
+            tron.B = TronDenseMatrix{Array{Float64, 2}}(n)
+            tron.L = TronDenseMatrix{Array{Float64, 2}}(n)
+        else
+            tron.B = TronDenseMatrix{CuArray{Float64, 2}}(n)
+            tron.L = TronDenseMatrix{CuArray{Float64, 2}}(n)
+        end
+        tron.nnz_a = n*n
+    end
+    tron.status = :NotSolved
+
+    return tron
+end
+
+function solveProblem(tron::ExaTronProblem)
+    task = Vector{UInt8}(undef, 60)
+    for (i,s) in enumerate("START")
+        task[i] = UInt8(s)
+    end
+
+    VI = Vector{Cint}
+    VD = typeof(tron.x)
+
+    isave = VI(undef, 3)
+    dsave = tron_zeros(VD, 3)
+    fatol = tron.options["fatol"]::Float64
+    frtol = tron.options["frtol"]::Float64
+    fmin = tron.options["fmin"]::Float64
+    cgtol = tron.options["cgtol"]::Float64
+    gtol = tron.options["tol"]::Float64
+    max_feval = tron.options["max_feval"]::Int
+    tcode = tron.options["tron_code"]::Symbol
+    max_minor = tron.options["max_minor"]::Int
+
+    # Project x into its bound. Tron requires x to be feasible.
+    tron.x .= max.(tron.x_l, min.(tron.x_u, tron.x))
+
+    itermax = tron.n
+    tron.minor_iter = 0
+    tron.nfev = tron.ngev = tron.nhev = 0
+    tron.status = :NotSolved
+    search = true
+
+    while (search)
+
+        # Evaluate the function.
+
+        if Char(task[1]) == 'F' || unsafe_string(pointer(task), 5) == "START"
+            tron.f = tron.eval_f(tron.x)
+            tron.nfev += 1
+            if tron.nfev >= max_feval
+                tron.status = :Maximum_Iterations_Exceeded
+                search = false
+            end
+        end
+
+        # Evaluate the gradient and Hessian.
+
+        if (Char(task[1]) == 'G' && Char(task[2]) == 'H') || unsafe_string(pointer(task), 5) == "START"
+            tron.eval_grad_f(tron.x, tron.g)
+            tron.eval_h(tron.x, :Values, Int[], Int[], 1.0, Float64[], tron.values)
+            tron.ngev += 1
+            tron.nhev += 1
+            tron.minor_iter += 1
+
+            # Copy values in the CSC matrix.
+            fill!(tron.A, 0.0)
+            transfer!(tron.A, tron.rows, tron.cols, tron.values, tron.nnz)
+        end
+
+        # Initialize the trust region bound.
+
+        if unsafe_string(pointer(task), 5) == "START"
+            gnorm0 = norm(tron.g)
+            tron.delta = gnorm0
+        end
+
+        # Call Tron.
+
+        if search
+            tron.delta = ExaSGDKernels.dtron(tron.n, tron.x, tron.x_l, tron.x_u,
+                            tron.f, tron.g, tron.A,
+                            frtol, fatol,
+                            fmin, cgtol, itermax, tron.delta,
+                            task, tron.B, tron.L,
+                            tron.xc, tron.s, tron.indfree,
+                            isave, dsave, tron.wa, tron.iwa)
+        end
+
+        if unsafe_string(pointer(task), 4) == "NEWX"
+            tron.gnorm_two, tron.gnorm_inf = gpnorm(tron.n, tron.x, tron.x_l, tron.x_u, tron.g)
+            if tron.gnorm_inf <= gtol
+                for (i,s) in enumerate("CONVERGENCE: GTOL TEST SATISFIED")
+                    task[i] = UInt8(s)
+                end
+            end
+
+            if tron.minor_iter >= max_minor
+                tron.status = :Maximum_Iterations_Exceeded
+                search = false
+            end
+        end
+
+        if unsafe_string(pointer(task), 4) == "CONV"
+            tron.status = :Solve_Succeeded
+            search = false
+        end
+
+        if unsafe_string(pointer(task), 4) == "WARN"
+            tron.status = :TooSmall
+            search = false
+        end
+    end
+
+    return tron.status
+end
